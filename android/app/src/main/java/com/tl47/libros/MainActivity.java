32ffc73e4c9fcafada731b615e35f2c80abc777d
Dv-AlejandroEH <aespher121@gmail.com>
2026-02-23 13:52:55 +0100

    itit

---
+++ android/app/src/main/java/com/tl47/libros/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tl47.libros;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tl47.libros',
+  appName: 'Libros',
+  webDir: 'www',
+};
+
+export default config;
